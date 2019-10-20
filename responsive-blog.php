bc48177117091c30cd20406c73620301d5fbd005
Arman hossain <armanh688@gmail.com>
2019-10-21 00:25:18 +0600

    responsive blog

---
+++ responsive-blog.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/slick.css">
+  <link rel="stylesheet" href="css/slick-theme.css>
+  <link rel="stylesheet" href="css/uikit.min.css">
+  <link rel="shortcut icon" type="image/x-icon" href="img/a.png">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+	<title>responsive blog</title>
+</head>
+<body>
+	<!-- start blog section -->
+<section id="blog" class='py-5'>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="card-columns">
+          <div class="card">
+            <img src="img/slider2.jpg" class='img-fluid card-img-top' alt="" />
+            <div class="card-body">
+              <div class="card-title">
+                <h4>Blog post one</h4>
+                <small class='text-muted'>Written by Arman hossain</small>
+                <hr />
+                <p class="lead">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque magnam ducimus ea quaerat corrupti harum in? Libero optio, magni ad.
+                </p>
+                <a href="#" class='btn btn-block btn-dark'>Read More</a>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img/slider5.jpg" class='img-fluid card-img-top' alt="" / height="236px">
+            <div class="card-body">
+              <div class="card-title">
+                <h4>Blog post Two</h4>
+                <small class='text-muted'>Written by Arman hossain</small>
+                <hr />
+                <p class="lead">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque magnam ducimus ea quaerat corrupti harum in? Libero optio, magni ad.
+                </p>
+                <a href="#" class='btn btn-block btn-dark'>Read More</a>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img/slider4.jpg" class='img-fluid card-img-top' alt="" />
+            <div class="card-body">
+              <div class="card-title">
+                <h4>Blog post Three</h4>
+                <small class='text-muted'>Written by Arman hossain</small>
+                <hr />
+                <p class="lead">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque magnam ducimus ea quaerat corrupti harum in? Libero optio, magni ad.
+                </p>
+                <a href="#" class='btn btn-block btn-dark'>Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- end blog section -->
+<script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/uikit.min.js"></script>
+  <script src="js/uikit-icons.min.js"></script>
+  <script src="js/slick.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
